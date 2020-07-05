cc8c11cdc8cd9b53d346b26db4a6a546329039a0
Minami <minami-git@minami-vr.sakura.ne.jp>
2020-07-05 16:32:17 +0900

    add momentum sgd

--- nn/optimizer.go
+++ nn/optimizer.go
@@ -8,12 +8,12 @@ type OptimizerFactory interface {
 	Create(Shape) Optimizer
 }
 
-type SGD struct {
-	LearningRate float64
+type sgd struct {
+	lr float64
 }
 
-func (s *SGD) Update(params, grads *Tensor) *Tensor {
-	params = params.SubTensor(grads.MulBroadCast(s.LearningRate))
+func (s *sgd) Update(params, grads *Tensor) *Tensor {
+	params = params.SubTensor(grads.MulBroadCast(s.lr))
 	return params
 }
 
@@ -22,5 +22,30 @@ type SGDFactory struct {
 }
 
 func (s *SGDFactory) Create(_ Shape) Optimizer {
-	return &SGD{LearningRate: s.LearningRate}
+	return &sgd{lr: s.LearningRate}
+}
+
+type momentumSGD struct {
+	lr       float64
+	momentum float64
+	velocity *Tensor
+}
+
+func (m *momentumSGD) Update(params, grads *Tensor) *Tensor {
+	m.velocity = m.velocity.MulBroadCast(m.momentum).SubTensor(grads.MulBroadCast(m.lr))
+	params = params.AddTensor(m.velocity)
+	return params
+}
+
+type MomentumSGDFactory struct {
+	LearningRate float64
+	Momentum     float64
+}
+
+func (m *MomentumSGDFactory) Create(shape Shape) Optimizer {
+	return &momentumSGD{
+		lr:       m.LearningRate,
+		momentum: m.Momentum,
+		velocity: NewTensor(shape),
+	}
 }
